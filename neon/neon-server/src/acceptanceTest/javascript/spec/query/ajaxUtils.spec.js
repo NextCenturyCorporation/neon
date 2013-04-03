81aacf53e1ad12aceecdc0836ffe3f3493933b4a
Jeff Storey <jeff.storey@nextcentury.com>
2013-04-03 19:13:46 -0400

    acceptance test for ajax timeouts

---
+++ neon/neon-server/src/acceptanceTest/javascript/spec/query/ajaxUtils.spec.js
@@ -0,0 +1,47 @@
+/*
+ * ************************************************************************
+ * Copyright (c), 2013 Next Century Corporation. All Rights Reserved.
+ *
+ * This software code is the exclusive property of Next Century Corporation and is
+ * protected by United States and International laws relating to the protection
+ * of intellectual property.  Distribution of this software code by or to an
+ * unauthorized party, or removal of any of these notices, is strictly
+ * prohibited and punishable by law.
+ *
+ * UNLESS PROVIDED OTHERWISE IN A LICENSE AGREEMENT GOVERNING THE USE OF THIS
+ * SOFTWARE, TO WHICH YOU ARE AN AUTHORIZED PARTY, THIS SOFTWARE CODE HAS BEEN
+ * ACQUIRED BY YOU "AS IS" AND WITHOUT WARRANTY OF ANY KIND.  ANY USE BY YOU OF
+ * THIS SOFTWARE CODE IS AT YOUR OWN RISK.  ALL WARRANTIES OF ANY KIND, EITHER
+ * EXPRESSED OR IMPLIED, INCLUDING, WITHOUT LIMITATION, IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE, ARE HEREBY EXPRESSLY
+ * DISCLAIMED.
+ *
+ * PROPRIETARY AND CONFIDENTIAL TRADE SECRET MATERIAL NOT FOR DISCLOSURE OUTSIDE
+ * OF NEXT CENTURY CORPORATION EXCEPT BY PRIOR WRITTEN PERMISSION AND WHEN
+ * RECIPIENT IS UNDER OBLIGATION TO MAINTAIN SECRECY.
+ */
+
+/**
+ * This is an end to end acceptance test to verify that queries can be executed against a mongo instance.
+ * These tests parallel those in the MongoQueryExecutorIntegrationTest.
+ */
+neon.query.SERVER_URL = 'http://localhost:10002/neon';
+
+describe('ajax utils', function () {
+
+    it('cancels ajax request', function () {
+        var statusText = "";
+        // the port comes from build.gradle
+        var request = neon.util.AjaxUtils.doGet('http://localhost:10008/neon/timeouttest', {
+            // we should get an error with an "abort" status
+            error: function (xhr, status, msg) {
+                statusText = xhr.statusText;
+            }
+        });
+        request.cancel();
+        waitsFor(function () {
+            return statusText === 'abort';
+        });
+    });
+
+});
