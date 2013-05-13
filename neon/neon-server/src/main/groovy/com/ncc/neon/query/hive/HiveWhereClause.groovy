b2510a922505bf2b36f3ee2619785158b5cc3672
Mike Tamayo <mike.tamayo@nextcentury.com>
2013-05-12 21:30:23 -0400

    NEON-153 HiveQL Integration

---
+++ neon/neon-server/src/main/groovy/com/ncc/neon/query/hive/HiveWhereClause.groovy
@@ -0,0 +1,78 @@
+/*
+ *
+ *  ************************************************************************
+ *  Copyright (c), 2013 Next Century Corporation. All Rights Reserved.
+ *
+ *  This software code is the exclusive property of Next Century Corporation and is
+ *  protected by United States and International laws relating to the protection
+ *  of intellectual property. Distribution of this software code by or to an
+ *  unauthorized party, or removal of any of these notices, is strictly
+ *  prohibited and punishable by law.
+ *
+ *  UNLESS PROVIDED OTHERWISE IN A LICENSE AGREEMENT GOVERNING THE USE OF THIS
+ *  SOFTWARE, TO WHICH YOU ARE AN AUTHORIZED PARTY, THIS SOFTWARE CODE HAS BEEN
+ *  ACQUIRED BY YOU "AS IS" AND WITHOUT WARRANTY OF ANY KIND. ANY USE BY YOU OF
+ *  THIS SOFTWARE CODE IS AT YOUR OWN RISK. ALL WARRANTIES OF ANY KIND, EITHER
+ *  EXPRESSED OR IMPLIED, INCLUDING, WITHOUT LIMITATION, IMPLIED WARRANTIES OF
+ *  MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE, ARE HEREBY EXPRESSLY
+ *  DISCLAIMED.
+ *
+ *  PROPRIETARY AND CONFIDENTIAL TRADE SECRET MATERIAL NOT FOR DISCLOSURE OUTSIDE
+ *  OF NEXT CENTURY CORPORATION EXCEPT BY PRIOR WRITTEN PERMISSION AND WHEN
+ *  RECIPIENT IS UNDER OBLIGATION TO MAINTAIN SECRECY.
+ * /
+ */
+
+package com.ncc.neon.query.hive
+
+import com.ncc.neon.query.clauses.AndWhereClause
+import com.ncc.neon.query.clauses.BooleanWhereClause
+import com.ncc.neon.query.clauses.OrWhereClause
+import com.ncc.neon.query.clauses.SingularWhereClause
+import com.ncc.neon.query.clauses.WhereClause
+
+/**
+ * Utility class for rendering Hive WHERE clauses
+ */
+class HiveWhereClause {
+
+    private WhereClause whereClause
+
+    private StringBuilder stringBuilder
+
+    @Override
+    public String toString() {
+        stringBuilder = new StringBuilder()
+
+        renderClause(whereClause)
+
+        return stringBuilder.toString()
+    }
+
+    private String renderClause(SingularWhereClause clause) {
+        stringBuilder << clause.lhs
+        stringBuilder << " " << clause.operator << " "
+        stringBuilder << ((clause.rhs instanceof String) ? "'" + clause.rhs + "'" : clause.rhs)
+    }
+
+    private String renderClause(BooleanWhereClause clause, String operator) {
+        stringBuilder << "("
+
+        clause.whereClauses.eachWithIndex { subClause, index ->
+            if (index > 0) {
+                stringBuilder << " " << operator << " "
+            }
+            renderClause(subClause)
+        }
+
+        stringBuilder << ")"
+    }
+
+    private String renderClause(AndWhereClause clause) {
+        renderClause(clause, "and")
+    }
+
+    private String renderClause(OrWhereClause clause) {
+        renderClause(clause, "or")
+    }
+}
