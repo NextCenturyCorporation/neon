a6acb11fb7500b4f90df35dc9850123683e73e98
Trevor Brooks <trevor.brooks@nextcentury.com>
2013-08-07 11:05:45 -0400

    Added some tests to make sure the mongo conversion is working

---
+++ tools/visualizations/next-century/neon/neon-server/src/test/groovy/com/ncc/neon/query/mongo/MongoConvertQueryTest.groovy
@@ -0,0 +1,96 @@
+package com.ncc.neon.query.mongo
+
+import com.mongodb.BasicDBObject
+import com.ncc.neon.query.Query
+import com.ncc.neon.query.clauses.SingularWhereClause
+import com.ncc.neon.query.filter.DataSet
+import com.ncc.neon.query.filter.Filter
+import com.ncc.neon.query.filter.FilterState
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+
+
+/*
+ * ************************************************************************
+ * Copyright (c), 2013 Next Century Corporation. All Rights Reserved.
+ *
+ * This software code is the exclusive property of Next Century Corporation and is
+ * protected by United States and International laws relating to the protection
+ * of intellectual property.  Distribution of this software code by or to an
+ * unauthorized party, or removal of any of these notices, is strictly
+ * prohibited and punishable by law.
+ *
+ * UNLESS PROVIDED OTHERWISE IN A LICENSE AGREEMENT GOVERNING THE USE OF THIS
+ * SOFTWARE, TO WHICH YOU ARE AN AUTHORIZED PARTY, THIS SOFTWARE CODE HAS BEEN
+ * ACQUIRED BY YOU "AS IS" AND WITHOUT WARRANTY OF ANY KIND.  ANY USE BY YOU OF
+ * THIS SOFTWARE CODE IS AT YOUR OWN RISK.  ALL WARRANTIES OF ANY KIND, EITHER
+ * EXPRESSED OR IMPLIED, INCLUDING, WITHOUT LIMITATION, IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE, ARE HEREBY EXPRESSLY
+ * DISCLAIMED.
+ *
+ * PROPRIETARY AND CONFIDENTIAL TRADE SECRET MATERIAL NOT FOR DISCLOSURE OUTSIDE
+ * OF NEXT CENTURY CORPORATION EXCEPT BY PRIOR WRITTEN PERMISSION AND WHEN
+ * RECIPIENT IS UNDER OBLIGATION TO MAINTAIN SECRECY.
+ *
+ * 
+ * @author tbrooks
+ */
+
+class MongoConvertQueryTest {
+
+    private FilterState filterState
+    private Filter simpleFilter
+    private Query simpleQuery
+
+    @Before
+    void setup() {
+        simpleFilter = new Filter(dataStoreName: DataSet.MONGO, databaseName: "test")
+        simpleQuery = new Query(filter: simpleFilter)
+        filterState = new FilterState()
+    }
+
+    @After
+    void teardown(){
+        simpleFilter = new Filter(dataStoreName: DataSet.MONGO, databaseName: "test")
+        simpleQuery = new Query(filter: simpleFilter)
+        filterState = new FilterState()
+    }
+
+    @Test(expected = NullPointerException)
+    void "a query to be converted must have a filter"() {
+        Query query = new Query()
+        whenExecutingConvertQuery(query)
+    }
+
+    @Test
+    void testSimplestConvertQuery() {
+        MongoQuery mongoQuery = whenExecutingConvertQuery(simpleQuery)
+
+        assert mongoQuery.query == simpleQuery
+        assert mongoQuery.whereClauseParams == new BasicDBObject()
+        assert mongoQuery.selectParams == null
+    }
+
+    @Test
+    void "test covertQuery does not care about the FilterState"() {
+        givenFilterStateHasOneFilter()
+        MongoQuery mongoQuery = whenExecutingConvertQuery(simpleQuery)
+
+        assert mongoQuery.query == simpleQuery
+        assert mongoQuery.whereClauseParams == new BasicDBObject()
+        assert mongoQuery.selectParams == null
+    }
+
+    private void givenFilterStateHasOneFilter() {
+        SingularWhereClause whereClause = new SingularWhereClause(lhs: "column", operator: "=", rhs: "test")
+        Filter filterWithWhere = new Filter(dataStoreName: simpleFilter.dataStoreName, databaseName: simpleFilter.databaseName, whereClause: whereClause)
+        filterState.addFilter(filterWithWhere)
+    }
+
+    private MongoQuery whenExecutingConvertQuery(Query query) {
+        MongoConversionStrategy conversionStrategy = new MongoConversionStrategy(filterState)
+        conversionStrategy.convertQuery(query)
+    }
+
+}
