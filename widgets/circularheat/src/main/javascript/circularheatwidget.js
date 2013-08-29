418e00d4b0aa2d658bc9e17a8c17f4c37a0b94a7
Trevor Brooks <trevor.brooks@nextcentury.com>
2013-08-29 10:39:56 -0400

    NEON-638 -- The heat chart is visible even when nothing is selected.

--- widgets/circularheat/src/main/javascript/circularheatwidget.js
+++ widgets/circularheat/src/main/javascript/circularheatwidget.js
@@ -51,6 +51,7 @@ $(document).ready(function () {
                 .radialLabels(["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"])
                 .segmentLabels(["12am", "1am", "2am", "3am", "4am", "5am", "6am", "7am", "8am", "9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm", "6pm", "7pm", "8pm", "9pm", "10pm", "11pm"])
                 .margin({top: 20, right: 20, bottom: 20, left: 20});
+            redrawChart();
         }
 
         function redrawChart() {
