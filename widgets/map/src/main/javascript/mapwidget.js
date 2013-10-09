117f2473281fc65a939ca308231eb0d6e7a4d1a0
Trevor Brooks <trevor.brooks@nextcentury.com>
2013-10-09 14:36:32 -0400

    NEON-751 -- The beginning of the core map widget. We can plot points here now.

--- widgets/map/src/main/javascript/mapwidget.js
+++ widgets/map/src/main/javascript/mapwidget.js
@@ -1,4 +1,13 @@
 $(function(){
-    var map = coreMap.Map("#map");
+    var map = new coreMap.Map("map");
+
+    var data = [
+        {latitude: 50, longitude: 0},
+        {latitude: 37, longitude: -117},
+        {latitude: 40, longitude: -96}
+    ];
+
+    map.setData(data);
+    map.draw();
 
 });
