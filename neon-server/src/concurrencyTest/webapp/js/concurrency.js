f0542c48806059fb3decd8bb0f93106e9f67ac01
Trevor Brooks <trevor.brooks@nextcentury.com>
2013-12-31 14:04:37 -0500

    NEON-919 -- Provide functionality to buttons.

--- neon-server/src/concurrencyTest/webapp/js/concurrency.js
+++ neon-server/src/concurrencyTest/webapp/js/concurrency.js
@@ -1,12 +1,68 @@
+var databaseName = 'acceptanceTest';
+var tableName = 'records';
+var filterKey;
 
 $(function(){
     neon.query.SERVER_URL = "http://localhost:11402/neon";
+    neon.query.registerForFilterKey(databaseName, tableName, function (filterResponse) {
+        filterKey = filterResponse;
+    });
+
+    $('#mongo').click(function(){
+        alert('mongo');
+    });
+
+    $('#hive').click(function(){
+        alert('hive');
+    });
+
+    $('#all-query').click(function(){
+        var query =  new neon.query.Query().selectFrom(databaseName, tableName);
+        query.allDataMode();
+        neon.query.executeQuery(query, function(data){
+            displayResults(JSON.stringify(data.data));
+        });
+    });
+
+    $('#filtered-query').click(function(){
+        var query =  new neon.query.Query().selectFrom(databaseName, tableName);
+        query.filteredMode();
+        neon.query.executeQuery(query, function(data){
+            displayResults(JSON.stringify(data.data));
+        });
+    });
+
+    $('#selection-query').click(function(){
+        var query =  new neon.query.Query().selectFrom(databaseName, tableName);
+        query.selectionMode();
+        neon.query.executeQuery(query, function(data){
+            displayResults(JSON.stringify(data.data));
+        });
+    });
+
+    $('#add-filter').click(function(){
+        var filter =  new neon.query.Filter().selectFrom(databaseName, tableName);
+        neon.query.addFilter(filterKey, filter);
+    });
+
+    $('#remove-filter').click(function(){
+        var filter =  new neon.query.Filter().selectFrom(databaseName, tableName);
+        neon.query.removeFilter(filterKey);
+    });
+
+    $('#add-selection').click(function(){
+        var selection =  new neon.query.Filter().selectFrom(databaseName, tableName);
+        neon.query.addSelection(filterKey, selection);
+    });
+
+    $('#remove-selection').click(function(){
+        var selection =  new neon.query.Filter().selectFrom(databaseName, tableName);
+        neon.query.removeSelection(filterKey);
+    });
+
 });
 
-
-function query(databaseName, tableName){
-    var query =  new neon.query.Query().selectFrom(databaseName, tableName);
-    neon.query.executeQuery(query, function(data){
-        $('#results').text(JSON.stringify(data.data));
-    });
+function displayResults(text){
+    $('#results').text(text);
 }
+
