f2309e5dd583bb17e92ab202ab54039bcf98dfa4
Jeff Storey <jeff.storey@nextcentury.com>
2014-01-30 17:36:34 -0500

    removing old header

--- neon-server/src/main/groovy/com/ncc/neon/query/filter/FilterCache.groovy
+++ neon-server/src/main/groovy/com/ncc/neon/query/filter/FilterCache.groovy
@@ -2,30 +2,7 @@ package com.ncc.neon.query.filter
 import com.ncc.neon.cache.ImmutableValueCache
 import com.ncc.neon.query.clauses.AndWhereClause
 import com.ncc.neon.query.clauses.WhereClause
-/*
- * ************************************************************************
- * Copyright (c), 2014 Next Century Corporation. All Rights Reserved.
- *
- * This software code is the exclusive property of Next Century Corporation and is
- * protected by United States and International laws relating to the protection
- * of intellectual property.  Distribution of this software code by or to an
- * unauthorized party, or removal of any of these notices, is strictly
- * prohibited and punishable by law.
- *
- * UNLESS PROVIDED OTHERWISE IN A LICENSE AGREEMENT GOVERNING THE USE OF THIS
- * SOFTWARE, TO WHICH YOU ARE AN AUTHORIZED PARTY, THIS SOFTWARE CODE HAS BEEN
- * ACQUIRED BY YOU "AS IS" AND WITHOUT WARRANTY OF ANY KIND.  ANY USE BY YOU OF
- * THIS SOFTWARE CODE IS AT YOUR OWN RISK.  ALL WARRANTIES OF ANY KIND, EITHER
- * EXPRESSED OR IMPLIED, INCLUDING, WITHOUT LIMITATION, IMPLIED WARRANTIES OF
- * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE, ARE HEREBY EXPRESSLY
- * DISCLAIMED.
- *
- * PROPRIETARY AND CONFIDENTIAL TRADE SECRET MATERIAL NOT FOR DISCLOSURE OUTSIDE
- * OF NEXT CENTURY CORPORATION EXCEPT BY PRIOR WRITTEN PERMISSION AND WHEN
- * RECIPIENT IS UNDER OBLIGATION TO MAINTAIN SECRECY.
- *
- * 
- */
+
 
 class FilterCache extends ImmutableValueCache<FilterKey, Filter> implements Serializable{
     private static final long serialVersionUID = - 5911927417066895555L
