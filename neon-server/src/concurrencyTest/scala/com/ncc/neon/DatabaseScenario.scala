e8bb52307f02312e9edf50f820d10519e658b62a
Trevor Brooks <trevor.brooks@nextcentury.com>
2014-02-11 14:14:36 -0500

    NEON-1006 - Fixed concurrency tests.

--- neon-server/src/concurrencyTest/scala/com/ncc/neon/DatabaseScenario.scala
+++ neon-server/src/concurrencyTest/scala/com/ncc/neon/DatabaseScenario.scala
@@ -7,6 +7,7 @@ import akka.util.duration._
 import bootstrap._
 import assertions._
 import Headers._
+import Requests._
 
 class DatabaseScenario extends Simulation {
 
@@ -18,14 +19,22 @@ class DatabaseScenario extends Simulation {
     .connection("keep-alive")
     .userAgentHeader("Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:23.0) Gecko/20100101 Firefox/23.0")
 
+  val serviceRoot = "/neon/services/"
+  val connectionId = "mongo@" + mongoHost
 
   val scn = scenario("Query for database and table names")
+    .exec(http("Create a connection")
+    .post(serviceRoot + "connections")
+    .headers(json_header)
+    .body(mongo_connection_request))
+    .pause(2)
+
     .exec(http("Get Database Names")
-    .get("http://localhost:11402/neon/services/queryservice/databasenames")
+    .get("http://localhost:11402/neon/services/queryservice/" + connectionId + "/databasenames")
     .check(jsonPath("$[?(@=='concurrencytest')]").exists))
 
     .exec(http("Get Table Names")
-    .post("http://localhost:11402/neon/services/queryservice/tablenames")
+    .post("http://localhost:11402/neon/services/queryservice/" + connectionId + "/tablenames")
     .headers(form_header)
     .param("database", "concurrencytest")
     .check(jsonPath("$[?(@=='records')]").exists))
