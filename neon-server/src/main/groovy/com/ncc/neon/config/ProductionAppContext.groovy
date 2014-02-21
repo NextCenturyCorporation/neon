b6766d4283d31982033ef3f260a0b58675546c52
Jeff Storey <jeff.storey@nextcentury.com>
2014-02-21 16:43:59 -0500

    removing old unused properties

--- neon-server/src/main/groovy/com/ncc/neon/config/ProductionAppContext.groovy
+++ neon-server/src/main/groovy/com/ncc/neon/config/ProductionAppContext.groovy
@@ -15,41 +15,17 @@
  */
 
 package com.ncc.neon.config
-
 import com.ncc.neon.transform.Transformer
 import com.ncc.neon.transform.TransformerRegistry
-import org.slf4j.Logger
-import org.slf4j.LoggerFactory
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Profile
-import org.springframework.context.annotation.PropertySource
-
-import javax.annotation.PostConstruct
 /**
  * Spring bean configuration to use in production
  */
 @Configuration
-@PropertySource("classpath:neon.properties")
 @Profile("production")
 class ProductionAppContext {
-
-    private static final Logger LOGGER = LoggerFactory.getLogger(ProductionAppContext)
-
-    @PostConstruct
-    @SuppressWarnings('JavaIoPackageAccess') // suppress to allow local file overrides
-    def postConstruct() {
-        LOGGER.trace("In post construct")
-        // TODO: NEON-89 Currently dev and production both use "production" as the environment (since we don't really have a production environment yet), which is why the override checks the user's home dir. In a real production environment, the file is likely to be somewhere else (such as /opt or /etc).  We can abstract this on a per environment basis.
-        def override = new File(System.getProperty("user.home"), "neon/neon-override.properties")
-        if (override.exists()) {
-            def properties = new Properties()
-            properties.load(new FileInputStream(override))
-            properties.each { prop, val ->
-                System.setProperty(prop, val)
-            }
-        }
-    }
 
     @Bean
     TransformerRegistry transformerRegistry() {
