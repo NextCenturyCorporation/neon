b9054dfb9559d8b54a663b665bd0078242f616ce
Jonathan Pautsch <jonathan.pautsch@nextcentury.com>
2014-04-09 13:47:24 -0400

    NEON-1058 Resized circular heat chart to fit left column

--- examples/angular-demo/app/js/directives/circularHeatChart.js
+++ examples/angular-demo/app/js/directives/circularHeatChart.js
@@ -35,8 +35,8 @@ angular.module('circularHeatChartDirective', []).directive('circularHeatChart', 
 
             // Initialize the chart.
             $scope.chart = new circularHeatChart(element[0])
-                .segmentHeight(20)
-                .innerRadius(20)
+                .segmentHeight(16)
+                .innerRadius(16)
                 .numSegments(24)
                 .radialLabels(["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"])
                 .segmentLabels(["12am", "1am", "2am", "3am", "4am", "5am", "6am", "7am", "8am", "9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm", "6pm", "7pm", "8pm", "9pm", "10pm", "11pm"])
