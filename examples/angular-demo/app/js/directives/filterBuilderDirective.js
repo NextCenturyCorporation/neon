599c256d8caffbcfa2ebb5f0555fa8d93331c7a0
lmizenextcen <lawerence.mize@nextcentury.com>
2014-04-11 14:29:00 -0400

    NEON-1062 Filling in the initial filtering capability.  It's enough to create and remove some basic AND filters and includes some neon extensions from the previous widget.  More UX logic and module cleanup to ensue.

--- examples/angular-demo/app/js/directives/filterBuilderDirective.js
+++ examples/angular-demo/app/js/directives/filterBuilderDirective.js
@@ -8,9 +8,123 @@ angular.module('filterBuilderDirective', []).directive('filterBuilder', ['Connec
 
 		},
 		link: function($scope, el, attr) {
-			var messenger = new neon.eventing.Messenger();
+			$scope.filterTable = new neon.query.FilterTable();
+			// Use a single session based filter key for this directive instance to allow multiple
+			// filter sets to be used in the same application.
+			$scope.filterTable.setFilterKey(neon.widget.getInstanceId());
+			$scope.selectedField = "Select Field";
+			$scope.andClauses = true;
+			$scope.selectedOperator = $scope.filterTable.operatorOptions[0] || '=';
 
-			
+			/**
+			 * Event handler for connection changed events issued over Neon's messaging channels.
+			 * @param {Object} message A Neon filter changed message.
+			 * @method onConnectionChanged
+			 * @private
+			 */ 
+			var onConnectionChanged = function(message) {
+				$scope.filterTable.clearFilterState();
+			};
+
+			/**
+			 * Event handler for dataset changed events issued over Neon's messaging channels.
+			 * @param {Object} message A Neon dataset changed message.
+			 * @param {String} message.database The database that was selected.
+			 * @param {String} message.table The table within the database that was selected.
+			 * @method onDatasetChanged
+			 * @private
+			 */ 
+			var onDatasetChanged = function(message) {
+				// Clear the filter table.
+				$scope.filterTable.clearFilterState();
+
+				// Save the new database and table name; Fetch the new table fields.
+				$scope.databaseName = message.database;
+				$scope.tableName = message.table;
+
+				connectionService.getActiveConnection().getFieldNames($scope.tableName, function(results) {
+				    $scope.$apply(function() {
+				        populateFieldNames(results);
+				        $scope.selectedField = results[0];
+				    });
+				});
+
+				// Clear our filters.
+				$scope.messenger.removeFilter($scope.filterTable.getFilterKey());
+
+			};
+
+			/** 
+			 * Initializes the name of the date field used to query the current dataset
+			 * and the Neon Messenger used to monitor data change events.
+			 * @method initialize
+			 */
+			$scope.initialize = function() {
+				$scope.messenger.events({
+					activeConnectionChanged: onConnectionChanged,
+					activeDatasetChanged: onDatasetChanged
+				});
+			};
+
+			/**
+			 * Adds a filter row to the table of filter clauses from the current selections.
+			 * @method addFilterRow
+			 */
+			$scope.addFilterRow = function() {
+				var row = new neon.query.FilterRow($scope.selectedField, $scope.selectedOperator, $scope.selectedValue);
+				$scope.filterTable.addFilterRow(row);
+
+				var filter = $scope.filterTable.buildFilterFromData($scope.databaseName, $scope.tableName, $scope.andClauses);
+				$scope.messenger.replaceFilter($scope.filterTable.filterKey, filter, function(){
+					//$scope.$apply();
+		        }, function() {
+		        	$scope.$apply(function() {
+			        	// Error handler:  the addition to the filter failed.  Remove it.
+			        	$scope.filterTable.removeFilterRow(filterTable.filterState.data.length - 1);
+
+			        	// TODO: Notify the user.
+			        });
+		        });
+			};
+
+			/**
+			 * Removes a filter row from the table of filter clauses.
+			 */
+			$scope.removeFilterRow = function(index) {
+
+				var row = $scope.filterTable.removeFilterRow(index);
+				// TODO: Make the neon call to remove it from the server.
+
+				var filter = $scope.filterTable.buildFilterFromData($scope.databaseName, $scope.tableName, $scope.andClauses);
+				$scope.messenger.replaceFilter($scope.filterTable.filterKey, filter, function(){
+					//$scope.$apply();
+		        }, function() {
+		        	$scope.$apply(function() {
+			        	// Error handler:  the addition to the filter failed.  Remove it.
+			        	$scope.filterTable.insertFilterRow(row, index);
+
+			        	// TODO: Notify the user.
+			        });
+		        });
+			}
+
+			/**
+			 * Helper method for setting the fields available for filter clauses.
+			 * @param {Array} fields An array of field name strings.
+			 * @method populateFieldNames
+			 * @private
+			 */
+			var populateFieldNames = function(fields) {
+				$scope.fields = fields;
+			};
+
+			$scope.onFilterAdd
+
+			// Wait for neon to be ready, the create our messenger and intialize the view and data.
+			neon.ready(function () {
+				$scope.messenger = new neon.eventing.Messenger();
+				$scope.initialize();
+			});
 		}
 	}
 }]);
---
+++ examples/angular-demo/app/js/neon-extensions/filtertable.js
@@ -0,0 +1,124 @@
+/*
+ * Copyright 2014 Next Century Corporation
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+var neon = neon || {};
+neon.query = neon.query || {};
+
+neon.query.FilterTable = function () {
+    //this.messenger = new neon.eventing.Messenger();
+    this.filterKey = '';
+    this.columnOptions;
+    this.operatorOptions = ["=", "!=", ">", "<", ">=", "<="];
+    this.filterState = {
+        data: []
+    };
+};
+
+neon.query.FilterRow = function (columnValue, operatorValue, value, columnOptions, operatorOptions) {
+    this.columnOptions = columnOptions;
+    this.columnValue = columnValue;
+    this.operatorOptions = operatorOptions;
+    this.operatorValue = operatorValue;
+    this.value = value;
+};
+
+neon.query.FilterTable.prototype.addFilterRow = function(row) {
+    this.filterState.data.push(row);
+}
+
+neon.query.FilterTable.prototype.insertFilterRow = function(row, index) {
+    this.filterState.data.splice(index, 1, row);
+}
+
+neon.query.FilterTable.prototype.removeFilterRow = function (id) {
+    return this.filterState.data.splice(id, 1);
+};
+
+neon.query.FilterTable.prototype.clearFilterState = function () {
+    this.filterState.data = [];
+};
+
+neon.query.FilterTable.prototype.setFilterKey = function (key) {
+    this.filterKey = key;
+};
+
+neon.query.FilterTable.prototype.setColumns = function (columns){
+    this.columnOptions = columns;
+};
+
+neon.query.FilterTable.prototype.getFilterKey = function () {
+    return this.filterKey;
+};
+
+neon.query.FilterTable.prototype.getFilterState = function () {
+    return this.filterState;
+};
+
+neon.query.FilterTable.prototype.buildFilterFromData = function(database, table, andClauses) {
+    var baseFilter = new neon.query.Filter().selectFrom(database, table);
+
+    var whereClause;
+    if (this.filterState.data.length === 0) {
+        return baseFilter;
+    }
+    if (this.filterState.data.length === 1) {
+        var filterData = this.filterState.data[0];
+        whereClause = neon.query.where(filterData.columnValue, filterData.operatorValue, neon.query.FilterTable.parseValue(filterData.value));
+    }
+    else {
+        whereClause = neon.query.FilterTable.buildCompoundWhereClause(this.filterState.data, andClauses);
+    }
+    return baseFilter.where(whereClause);
+};
+
+neon.query.FilterTable.buildCompoundWhereClause = function(data, andClauses) {
+    var whereClause;
+    var clauses = [];
+
+    $.each(data, function (index, filterData) {
+        var clause = neon.query.where(filterData.columnValue, filterData.operatorValue, neon.query.FilterTable.parseValue(filterData.value));
+        clauses.push(clause);
+    });
+
+    if (andClauses) {
+        whereClause = neon.query.and.apply(this, clauses);
+    }
+    else {
+        whereClause = neon.query.or.apply(this, clauses);
+    }
+    return whereClause;
+};
+
+neon.query.FilterTable.parseValue = function(value) {
+    var retVal = value;
+
+    if ($.isNumeric(retVal)) {
+       retVal = parseFloat(retVal);
+    }
+    else if (retVal === "null" || retVal === "") {
+       retVal = null;
+    }
+    else if (retVal === '""') {
+       retVal = "";
+    }
+    else if (retVal === 'false') {
+       retVal = false;
+    }
+    else if (retVal === 'true') {
+       retVal = true;
+    }
+    return retVal;
+};
