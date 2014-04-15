b3371f98bab903414c96f1d07a692e1fa0c412ca
lmizenextcen <lawerence.mize@nextcentury.com>
2014-04-15 16:41:38 -0400

    NEON-1061 Cleaning up some temp code and shuffling around some CSS styles.

--- examples/angular-demo/app/js/charts/timelineSelectorChart.js
+++ examples/angular-demo/app/js/charts/timelineSelectorChart.js
@@ -17,8 +17,7 @@ charts.TimelineSelectorChart = function (element, configuration)
 	this.configure = function(configuration)
 	{
 		this.config = configuration || {};
-		this.config.margin = this.config.margin || {top: 0, right: 0, bottom: 20, left: 0};
-		this.config.margin2 = this.config.margin2 || {top: 0, right: 0, bottom: 0, left: 0};
+		this.config.margin = this.config.margin || {top: 10, right: 0, bottom: 20, left: 40};
 		this.config.width = this.config.width  || 800;
 		this.config.height = this.config.height || 50;
 
@@ -33,23 +32,17 @@ charts.TimelineSelectorChart = function (element, configuration)
 			data = values;
 		}
 
-		// Clear the old data. 
-		// d3.select(this.element)
-  //           .selectAll('svg')
-  //           .remove();
-
 		var parseDate = d3.time.format("%b %Y").parse;
 
 		var x = d3.time.scale().range([0, this.config.width]),
 		    y = d3.scale.linear().range([this.config.height, 0]);
 
 		var xAxis = d3.svg.axis().scale(x).orient("bottom"),
-		    yAxis = d3.svg.axis().scale(y).orient("left");
+		    yAxis = d3.svg.axis().scale(y).orient("left").ticks(1);
 
 		var brush = d3.svg.brush()
 		    .x(x)
-		    .on("brush", brushed);
-		 //   .on("mouseup", function() { console.log("LET GO!"); });
+		    .on("brushend", brushed);
 
 		var area = d3.svg.area()
 		    .interpolate("monotone")
@@ -57,16 +50,21 @@ charts.TimelineSelectorChart = function (element, configuration)
 		    .y0(this.config.height)
 		    .y1(function(d) { return y(d.value); });
         
+        // var yMin = d3.min(data.map(function(d) { return d.value; }));
+        // var yMax = d3.max(data.map(function(d) { return d.value; }));
+
         x.domain(d3.extent(data.map(function(d) { return d.date; })));
 		y.domain([0, d3.max(data.map(function(d) { return d.value; }))]);
 
+        // Clear the old contents by replacing innerhtml with the summary header.
 		d3.select(this.element).html('<h3><span class="count count-large text-primary">' 
 			+ data.length
 			+ '</span> records from February 1, 2014 to March 31, 2014</h3>');
 
+        // Append our chart graphics
 		var svg = d3.select(this.element)
 		    .attr("class", "timeline-selector-chart")
-		    .append("svg")
+		  .append("svg")
 		    .attr("viewBox","0 0 "
 		    	+ (this.config.width + this.config.margin.left + this.config.margin.right) + " "
 		    	+ (this.config.height + this.config.margin.bottom + this.config.margin.top));
@@ -81,27 +79,26 @@ charts.TimelineSelectorChart = function (element, configuration)
 		    .attr("class", "context")
 		    .attr("transform", "translate(" + this.config.margin.left + "," + this.config.margin.top + ")");
 
-		//d3.csv("sp500.csv", type, function(error, data) {
-		  // x.domain(d3.extent(data.map(function(d) { return d.date; })));
-		  // y.domain([0, d3.max(data.map(function(d) { return d.value; }))]);
+		context.append("path")
+		    .datum(data)
+		    .attr("class", "area")
+		    .attr("d", area);
 
-		  context.append("path")
-		      .datum(data)
-		      .attr("class", "area")
-		      .attr("d", area);
+		context.append("g")
+		    .attr("class", "x axis")
+		    .attr("transform", "translate(0," + this.config.height + ")")
+		    .call(xAxis);
 
-		  context.append("g")
-		      .attr("class", "x axis")
-		      .attr("transform", "translate(0," + this.config.height + ")")
-		      .call(xAxis);
+		context.append("g")
+			.attr("class", "y axis")
+			.call(yAxis);
 
-		  context.append("g")
-		      .attr("class", "x brush")
-		      .call(brush)
-		    .selectAll("rect")
-		      .attr("y", -6)
-		      .attr("height", this.config.height + 7);
-		//});
+		context.append("g")
+		    .attr("class", "x brush")
+		    .call(brush)
+		  .selectAll("rect")
+		    .attr("y", -6)
+		    .attr("height", this.config.height + 7);
 
 		function brushed() {
 			console.log("WE BRUSHED!");
