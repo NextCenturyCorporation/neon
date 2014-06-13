fb6afe5adcad71b8311ac6b8476bbb614461397a
lmizenextcen <lawerence.mize@nextcentury.com>
2014-06-13 14:18:35 -0400

    NEON-1145: Stubbing out XDATA logging in the Neon demo.

---
+++ examples/angular-demo/app/js/xdata/xdataConfig.js
@@ -0,0 +1,23 @@
+'use strict';
+/*
+ * Copyright 2014 Next Century Corporation
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+ var XDATA = {};
+
+ XDATA.ACTIVITY_LOGGER_URL = "http://xd-draper.xdata.data-tactics-corp.com:1337";
+ XDATA.COMPONENT = "Neon Demo";
+ XDATA.COMPONENT_VERSION = "0.8.0-SNAPSHOT";
+
+ XDATA.activityLogger = new activityLogger().echo(true).testing(true);
