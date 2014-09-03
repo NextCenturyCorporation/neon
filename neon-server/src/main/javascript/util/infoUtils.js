85525a97f42b91972d8d836b360165452718c6d8
Clark Dorman <clark.dorman@nextcentury.com>
2014-09-03 11:01:56 -0400

    Implement js to return neon version

---
+++ neon-server/src/main/javascript/util/infoUtils.js
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2013 Next Century Corporation
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+
+/**
+ * Provides utility methods for getting information about neon
+ * @class neon.util.infoUtils
+ * @static
+ */
+
+neon.util.infoUtils = (function () {
+
+    /*
+     * Gets the version information for Neon
+     * @method getNeonVersion
+     * @return {neon.util.AjaxRequest}
+     */
+    function getNeonVersion(successCallback) {
+        return neon.util.ajaxUtils.doGet(
+            neon.serviceUrl('infoservice', 'version'), {
+                success: successCallback,
+                responseType: 'text'
+            }
+        );
+    }
+
+    return {
+        getNeonVersion: getNeonVersion
+    };
+
+})();
+
