a0ca19f1e06a0f07ec4d5fdcde226ea45a52cf65
Josh Williams <halfs13@gmail.com>
2014-09-11 01:03:08 -0400

    Fixing filter communication

--- app/js/app.js
+++ app/js/app.js
@@ -1,7 +1,11 @@
+'use strict';
+neon.SERVER_URL = "/neon";
+
+
 angular.module("memex", [
 	'neon.controllers',
 	'neon.services',
-
+	'neon.directives',
 
 	'circularHeatChartDirective',
 	'circularHeatFormDirective',
