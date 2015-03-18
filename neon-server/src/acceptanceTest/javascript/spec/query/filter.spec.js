f452986b0b84dc8124b221e8acdc302a77d37cc6
Josh Williams <halfs13@gmail.com>
2015-03-18 10:58:32 -0400

    NEON-725 properly handling async filter clear in jasmine beforeEach

--- neon-server/src/acceptanceTest/javascript/spec/query/filter.spec.js
+++ neon-server/src/acceptanceTest/javascript/spec/query/filter.spec.js
@@ -20,58 +20,131 @@
  */
 
  /*global host*/
- /*global getJSONFixture*/
  /*global neontest*/
+
 describe('neon.query.Filter', function() {
-    var databaseName = 'acceptanceTest';
-    var tableName = 'records';
-    // var allData;
-
-    // var dcStateFilter = baseFilter().where('state', '=', 'DC');
-
-    // jasmine.getJSONFixtures().fixturesPath = 'src/test-data';
+    var databaseName = 'db1';
+    var tableName = 't1';
 
     var connection = new neon.query.Connection();
-    // var messenger = new neon.eventing.Messenger();
-    // var filterId = "filterA";
+    var messenger = new neon.eventing.Messenger();
 
-    // helpers to run async tests against the correct objects
     var executeCallAndWait = function(functionObj, asyncFunction, params) {
         return neontest.executeAndWait(functionObj, asyncFunction, params);
     };
 
-    // var sendMessageAndWait = function(asyncFunction, args) {
-    //     return neontest.executeAndWait(messenger, asyncFunction, args);
-    // };
-
     beforeEach(function() {
-        // host is generated dynamically during the build and included in the acceptance test helper file
         connection.connect(neon.query.Connection.MONGO, host);
-        connection.use(databaseName);
+        var filterClearComplete = false;
 
-        // if (!allData) {
-        //     allData = getJSONFixture('data.json');
-        // }
+        runs(function() {
+            messenger.clearFiltersSilently(function() {
+                filterClearComplete = true;
+            });
+        });
+
+        waitsFor(function() {
+            return filterClearComplete;
+        });
     });
-
-    /**
-     * Returns a filter that is configured to select from the test data source
-     * @return {neon.query.Filter}
-     */
-    // function baseFilter() {
-    //     return new neon.query.Filter().selectFrom(databaseName, tableName);
-    // }
 
     function assertAsyncResults(functionObj, asyncFunction, params, expectedData) {
         var result = executeCallAndWait(functionObj, asyncFunction, params);
         runs(function() {
-            expect(result.get().data).toEqual(expectedData);
+            expect(result.get()).toEqual(expectedData);
         });
     }
 
+    var addFilters = function(callback) {
+        var filter = new neon.query.Filter().selectFrom("db1", "t1").where("a", "=", 1);
+        var filter2 = new neon.query.Filter().selectFrom("db1", "t2").where("b", "=", 2);
+        var filter3 = new neon.query.Filter().selectFrom("db2", "t1").where("c", "=", 3);
+        var filter4 = new neon.query.Filter().selectFrom("db2", "t3").where("d", "=", 4);
+
+        neontest.executeAndWait(messenger, messenger.addFilter, [uuid(), filter]);
+        runs(function() {
+            neontest.executeAndWait(messenger, messenger.addFilter, [uuid(), filter2]);
+            runs(function() {
+                neontest.executeAndWait(messenger, messenger.addFilter, [uuid(), filter3]);
+                runs(function() {
+                    neontest.executeAndWait(messenger, messenger.addFilter, [uuid(), filter4]);
+                    runs(function() {
+                        callback();
+                    });
+                });
+            });
+        });
+    };
+
     describe("getFilterState", function() {
-        it("should return empty results when no filters are set", function() {
-            assertAsyncResults(neon.query.Filter, neon.query.Filter.getFilterState, [databaseName, tableName], []);
+        it('should return all filters for table * and database *', function() {
+            runs(function() {
+                addFilters(function() {
+                    var result = executeCallAndWait(this, neon.query.Filter.getFilterState, ["*", "*"]);
+                    runs(function() {
+                        result = result.get();
+                        expect(result.length).toBe(4);
+                    });
+                });
+            });
+        });
+
+        it('should return filters for all tables for table * and database', function() {
+            runs(function() {
+                addFilters(function() {
+                    var result = executeCallAndWait(this, neon.query.Filter.getFilterState, [databaseName, "*"]);
+                    runs(function() {
+                        result = result.get();
+                        expect(result.length).toBe(2);
+                        expect(result[0].databaseName).toBe(databaseName);
+                        expect(result[1].databaseName).toBe(databaseName);
+                    });
+                });
+            });
+        });
+
+        it('should return filters for all databases for table and database *', function() {
+            runs(function() {
+                addFilters(function() {
+                    var result = executeCallAndWait(this, neon.query.Filter.getFilterState, ["*", tableName]);
+                    runs(function() {
+                        result = result.get();
+                        expect(result.length).toBe(2);
+                        expect(result[0].tableName).toBe(tableName);
+                        expect(result[0].tableName).toBe(tableName);
+                    });
+                });
+            });
+        });
+
+        it('should return filters for database and table', function() {
+            runs(function() {
+                addFilters(function() {
+                    var result = executeCallAndWait(this, neon.query.Filter.getFilterState, [databaseName, tableName]);
+                    runs(function() {
+                        result = result.get();
+                        expect(result.length).toBe(1);
+                        expect(result[0].databaseName).toBe(databaseName);
+                        expect(result[0].tableName).toBe(tableName);
+                    });
+                });
+            });
+        });
+
+        it('should return empty array if no filters exist for table and database', function() {
+            assertAsyncResults(this, neon.query.Filter.getFilterState, [databaseName, tableName], []);
+        });
+
+        it('should return empty array if no filters exist for table * and database', function() {
+            assertAsyncResults(this, neon.query.Filter.getFilterState, [databaseName, "*"], []);
+        });
+
+        it('should return empty array if no filters exist for table and database *', function() {
+            assertAsyncResults(this, neon.query.Filter.getFilterState, ["*", tableName], []);
+        });
+
+        it('should return empty array if no filters exist for table * and database *', function() {
+            assertAsyncResults(this, neon.query.Filter.getFilterState, ["*", "*"], []);
         });
     });
 });
