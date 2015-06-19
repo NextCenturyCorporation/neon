7637df1bbdf3369f09d4c941c13c20b7982dd704
Clark Dorman <clark.dorman@nextcentury.com>
2015-06-19 06:52:26 -0400

    Keep track of cache hits and misses; print out periodically

--- neon-server/src/main/groovy/com/ncc/neon/query/mongo/SimpleQueryCache.groovy
+++ neon-server/src/main/groovy/com/ncc/neon/query/mongo/SimpleQueryCache.groovy
@@ -16,6 +16,7 @@
 package com.ncc.neon.query.mongo
 
 import com.ncc.neon.query.result.QueryResult
+import org.apache.commons.jcs.engine.control.CompositeCacheManager
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.apache.commons.jcs.JCS
@@ -35,10 +36,8 @@ import org.apache.commons.jcs.access.exception.CacheException
  * TODO:
  *    -- Use Tomcat to manage this rather than simple singleton pattern
  *    -- Manage memory used, or at least figure out how much it is using
- *    -- Use disk cache
  *    -- invalidate cache when data changes
  *    -- re-pull cache when restarted or cache invalidated
- *    -- prioritize cache
  *    -- turn caching on / off dynamically
  *    -- (?) Track cache hits and misses and adjust num objects stored
  */
@@ -47,6 +46,22 @@ public class SimpleQueryCache {
     private static final Logger LOGGER = LoggerFactory.getLogger(SimpleQueryCache)
 
     private CacheAccess<String, QueryResult> cache = null
+
+    // Our cache name.
+    private final String cacheName = "default"
+
+    // ------------------------------------------------------------
+    // Static settings
+    // ------------------------------------------------------------
+    // time limit for queries, in sec.  If they are fast enough, do not put them in cache, so that
+    // the most important queries can be saved.  Currently really high, so cache everything
+    static final double QUERY_TIME_LIMIT = 9999999.0
+
+    // How often to print; every X times a query is 'get'.  Want 
+    static final int PRINTLIMIT = 20;
+
+    int keyMiss = 0
+    int keyHit = 0
 
     // use internal holder object to hold the instance
     // See: https://www.securecoding.cert.org/confluence/display/java/MSC07-J.+Prevent+multiple+instantiations+of+singleton+objects
@@ -64,7 +79,7 @@ public class SimpleQueryCache {
 
     void initializeCache() {
         try {
-            cache = JCS.getInstance("default")
+            cache = JCS.getInstance(cacheName)
         }
         catch (CacheException e) {
             LOGGER.error("Problem initializing cache: ", e.getMessage())
@@ -72,16 +87,24 @@ public class SimpleQueryCache {
     }
 
     QueryResult get(MongoQuery mongoQuery) {
-        if(!cache) {
+        if (!cache) {
             return null
         }
         String queryString = mongoQuery.toString()
         QueryResult result = cache.get(queryString)
+        if (result == null) {
+            keyMiss++
+            LOGGER.debug("Cache miss")
+        } else {
+            keyHit++
+            LOGGER.debug("Cache hit")
+        }
+        showSummaryStatistics()
         return result
     }
 
     void put(MongoQuery mongoQuery, QueryResult result) {
-        if(!cache) {
+        if (!cache) {
             return
         }
         String queryString = mongoQuery.toString()
@@ -93,4 +116,25 @@ public class SimpleQueryCache {
         }
     }
 
+    void put(MongoQuery mongoQuery, QueryResult result, double queryTime) {
+        if (queryTime < QUERY_TIME_LIMIT) {
+            return
+        }
+        put(mongoQuery, result)
+    }
+
+    /**
+     * Try to figure out what the queries are, what they were doing, how often they hit versus miss
+     */
+    void showSummaryStatistics() {
+        if ((keyMiss + keyHit) % 20 != 0) {
+            return
+        }
+        LOGGER.error(" Cache hits / misses: " + keyHit + " " + keyMiss + " (" + (100 * keyHit / (keyHit + keyMiss)) + "%)")
+
+        def keys = CompositeCacheManager.getInstance().getCache(cacheName).getMemoryCache().getKeySet()
+        for (String key : keys) {
+            LOGGER.error(" Key:  " + key)
+        }
+    }
 }
