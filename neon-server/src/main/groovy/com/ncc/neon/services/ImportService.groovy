a94b3e4bb64857df5acce64c1c313c3c6662722c
Daniel Gilley <daniel.gilley@nextcentury.com>
2015-07-06 15:18:44 -0400

    Got ImportService.groovy reading input using a stream, so now it shouldn't instantly fail on large files.

--- neon-server/src/main/groovy/com/ncc/neon/services/ImportService.groovy
+++ neon-server/src/main/groovy/com/ncc/neon/services/ImportService.groovy
@@ -19,11 +19,11 @@ package com.ncc.neon.services
 import org.springframework.stereotype.Component
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.RequestParam
-//import org.springframework.web.multipart.MultipartFile
 import org.apache.commons.fileupload.MultipartStream
 import org.apache.commons.fileupload.servlet.ServletFileUpload
 import com.sun.jersey.multipart.MultiPart
 import com.sun.jersey.multipart.BodyPart
+import com.sun.jersey.multipart.FormDataParam
 
 import javax.ws.rs.*
 import javax.ws.rs.core.MediaType
@@ -38,27 +38,33 @@ import groovy.json.JsonOutput
 @Path("/importservice")
 class ImportService {
 
-//    @Autowired
-//    CommonsMultipartResolver resolver
-
 	@POST
+    @Consumes(MediaType.MULTIPART_FORM_DATA)
     @Produces(MediaType.APPLICATION_JSON)
     @Path("upload/{host}/{databaseType}")
     public String getFile(@PathParam("host") String host,
                           @PathParam("databaseType") String databaseType,
-                          @RequestParam("file") MultiPart data) {
+                          @FormDataParam("file") InputStream dataInputStream) {
 
-        /* From Jersey. Worked for smaller files, failed for > ~10 mB.
-        BodyPart part0 = data.getBodyParts().get(0)
-        String s = part0.getEntityAs(String.class)
-        //System.out.println(s)
+        /*
+         * From Jersey. Uses a stream, so appears to work for even very large files. I tested by giving it a 363.4 mB file and it worked fine.
+         * I obviously wasn't processing the whole thing, but I see no reason why it should fail if I do, given the use of a stream to get data.
+         */
+        byte[] buff = new byte[24]
+        dataInputStream.read(buff)
+        byte[] buff2 = new byte[24]
+        dataInputStream.read(buff2)
+        String s = new String(buff)
+        s = s + new String(buff2)
         Map m = [data: s]
-        return JsonOutput.toJson(m) */
 
+        /*
+         * This tried to use the Apache fileupload library, but it didn't work - kept saying there was no reader for multipart form data.
         ByteArrayInputStream bStream = new ByteArrayInputStream(data.getBytes())
         @SuppressWarnings("deprecated")
         MultipartStream mStream = new MultipartStream(bStream, "---------------------------123206693813157131941135385742".toByteArray(), 1024, new MultipartStream.ProgressNotifier())
         Map m = [data: ServletFileUpload.isMultipartContent(data)]
+        */
         return JsonOutput.toJson(m)
     }
 }
