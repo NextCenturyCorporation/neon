019588c85baa82e0e1527d89ba5729707455d626
Rob Szewczyk <rob.szew@gmail.com>
2015-08-28 16:17:55 -0400

    NEON-1651 Implementing getArrayCounts and groupBy statement

--- neon-server/src/main/groovy/com/ncc/neon/query/elasticsearch/ElasticSearchQueryExecutor.groovy
+++ neon-server/src/main/groovy/com/ncc/neon/query/elasticsearch/ElasticSearchQueryExecutor.groovy
@@ -18,11 +18,14 @@ package com.ncc.neon.query.elasticsearch
 
 import com.ncc.neon.connect.ConnectionManager
 import com.ncc.neon.query.clauses.AndWhereClause
+import com.ncc.neon.query.clauses.GroupByFieldClause
+import com.ncc.neon.query.clauses.GroupByFunctionClause
 import com.ncc.neon.query.clauses.OrWhereClause
 import com.ncc.neon.query.clauses.SelectClause
 import com.ncc.neon.query.clauses.SingularWhereClause
 import com.ncc.neon.query.executor.AbstractQueryExecutor
 import com.ncc.neon.query.filter.DataSet
+import com.ncc.neon.query.filter.Filter
 import com.ncc.neon.query.filter.FilterState
 import com.ncc.neon.query.filter.SelectionState
 import com.ncc.neon.query.result.ArrayCountPair
@@ -30,34 +33,34 @@ import com.ncc.neon.query.result.QueryResult
 import com.ncc.neon.query.QueryOptions
 import com.ncc.neon.query.Query
 import com.ncc.neon.query.result.TabularQueryResult
-import org.elasticsearch.index.query.QueryBuilders
+
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Component
 
+import org.elasticsearch.index.query.FilterBuilder
+import org.elasticsearch.index.query.FilterBuilders
+import org.elasticsearch.index.query.QueryBuilders
 import org.elasticsearch.action.search.SearchRequest
 import org.elasticsearch.action.search.SearchType
 import org.elasticsearch.search.sort.SortBuilders
 import org.elasticsearch.search.sort.SortOrder
 import org.elasticsearch.search.builder.SearchSourceBuilder
-import org.elasticsearch.common.collect.ImmutableOpenMap
-import org.elasticsearch.client.Client
-import org.elasticsearch.index.query.FilterBuilder
-import org.elasticsearch.index.query.FilterBuilders
 import org.elasticsearch.search.aggregations.AggregationBuilders
 import org.elasticsearch.search.aggregations.AbstractAggregationBuilder
-
-import org.elasticsearch.cluster.metadata.IndexMetaData
+import org.elasticsearch.common.collect.ImmutableOpenMap
+import org.elasticsearch.client.Client
+import org.elasticsearch.search.aggregations.bucket.histogram.DateHistogram.Interval
+import org.elasticsearch.search.aggregations.bucket.MultiBucketsAggregation
 import org.elasticsearch.action.admin.cluster.state.ClusterStateRequest
-import org.elasticsearch.action.admin.cluster.state.ClusterStateResponse
 import org.elasticsearch.action.admin.indices.mapping.get.GetMappingsRequest
-import org.elasticsearch.action.admin.indices.mapping.get.GetMappingsResponse
-import org.elasticsearch.cluster.metadata.MappingMetaData
 
 @Component
 class ElasticSearchQueryExecutor extends AbstractQueryExecutor {
+
+    static final String STATS_AGG_PREFIX = "_statsFor_"
 
     private static final Logger LOGGER = LoggerFactory.getLogger(ElasticSearchQueryExecutor)
 
@@ -89,34 +92,49 @@ class ElasticSearchQueryExecutor extends AbstractQueryExecutor {
         def inners = whereClauses.collect(ElasticSearchQueryExecutor.&resolveWhereClause) as FilterBuilder[]
         def whereFilter = FilterBuilders.boolFilter().must(FilterBuilders.andFilter(inners))
 
-        //Build the elasticsearch aggregations - note that count is handled as a special case
-        def aggregations = query.aggregates
-            .findAll({ it.operation != 'count' })
-            .collect(ElasticSearchQueryExecutor.&resolveSingleValueAgg) as AbstractAggregationBuilder[]
-
         //Build the sorters for the sort clause
         def sorters = (query.sortClauses ?: []).collect(ElasticSearchQueryExecutor.&resolveSortClause)
 
-
-        //Build the SearchQuery
-        def source = new SearchSourceBuilder()
-            .query(QueryBuilders.filteredQuery(null, whereFilter))
-            .explain(false)
-            .from((query.offsetClause ? query.offsetClause.offset : 0) as int)
-            .size((query.limitClause ? query.limitClause.limit : 45) as int)
+        //begin constructing the search
+        def source = createSearchSourceBuilder(query).query(QueryBuilders.filteredQuery(null, whereFilter))
 
         if (query.fields && query.fields != SelectClause.ALL_FIELDS) {
             source.fetchSource(query.fields as String[])
         }
 
-        sorters.each { source.sort it }
-        aggregations.each { source.aggregation it }
+        sorters.each(source.&sort)
 
-        //Build the request
-        def request = new SearchRequest()
-            .searchType(SearchType.DFS_QUERY_THEN_FETCH)
-            .source(source)
-            .indices(query.filter?.databaseName ?: '_all')
+        //Build the metric aggregations - remove the count all aggregation since we can get that as part of the
+        //search result; also, rather than pass individual metrics for each aggregation field, we'll simply
+        //get stats on each field for which an aggregation exists thereby getting everything
+        def aggregates = query.aggregates
+        def aggregations = aggregates
+            .findAll({ !isCountAllAggregation(it) })
+            .collect({ it.field })
+            .unique()
+            .collect({ AggregationBuilders.stats("${STATS_AGG_PREFIX}${it}").field(it as String) })
+
+        //build the groupBy statement
+        def groupByClauses = query.groupByClauses
+        if (groupByClauses) {
+            def groupByAggregationBuilders = groupByClauses.collect(ElasticSearchQueryExecutor.&resolveGroupByClause)
+
+            //apply aggregations to the terminal group by clause
+            aggregations.each { groupByAggregationBuilders.last().subAggregation(it) }
+
+            //on each aggregation, except the last - nest the next aggregation
+            groupByAggregationBuilders
+                .take(groupByAggregationBuilders.size() - 1)
+                .eachWithIndex({ v, i -> v.subAggregation(groupByAggregationBuilders[i + 1]) })
+
+            source.aggregation(groupByAggregationBuilders.head() as AbstractAggregationBuilder)
+
+        } else {
+            //if there are no groupByClauses, apply metrics aggregations directly
+            aggregations.each(source.&aggregation)
+        }
+
+        def request = createSearchRequest(source, query)
 
         if (query.filter?.tableName) {
             request.types([query.filter.tableName] as String[])
@@ -125,22 +143,14 @@ class ElasticSearchQueryExecutor extends AbstractQueryExecutor {
         //and...  search!
         def results = getClient().search(request).actionGet()
 
-        if (query.aggregates?.size() > 0) {
-            def aggs = results.aggregations?.asList()?.collect { it ->
-                def aggMap = new HashMap<String, Object>()
-                aggMap.put(it.name, it.value)
-                aggMap
-            } ?: []
+        def aggResults = results.aggregations
 
-            def countAgg = query.aggregates.find({ it.operation == 'count'})
+        if (aggregates && !groupByClauses) {
+            return new TabularQueryResult(extractMetricAggregations(aggregates, aggResults.asMap(), results.hits.totalHits))
+        }
 
-            if (countAgg) {
-                def countAggMap = new HashMap<String, Object>()
-                countAggMap.put(countAgg.name, results.hits.totalHits)
-                aggs << countAggMap
-            }
-
-            return new TabularQueryResult(aggs)
+        if (groupByClauses) {
+            return new TabularQueryResult(extractGroupByAggregation(groupByClauses, aggResults.asList()[0], aggregates, results.hits.totalHits))
         }
 
         new TabularQueryResult(results.hits.collect { it.getSource() })
@@ -149,40 +159,49 @@ class ElasticSearchQueryExecutor extends AbstractQueryExecutor {
     @Override
     List<String> showDatabases() {
         LOGGER.debug("Executing showDatabases to retrieve indices")
-        Client client = getClient()
-
-        ImmutableOpenMap<String, IndexMetaData> indecesMap = client.admin().cluster().state(new ClusterStateRequest())
-             .actionGet().getState().getMetaData().indices()
-
-        return Arrays.asList(indecesMap.keys().toArray(String));
+        getClient().admin().cluster().state(new ClusterStateRequest()).actionGet().state.metaData.indices.collect { it.key }
     }
 
     @Override
     List<String> showTables(String dbName) {
         LOGGER.debug("Executing showTables for index " + dbName + " to get type mappings")
-        Client client = getClient()
-
-        GetMappingsResponse mappingResponse = client.admin().indices().getMappings(new GetMappingsRequest().indices(dbName)).get()
-        ImmutableOpenMap<String, MappingMetaData> typesMap = mappingResponse.mappings().get(dbName)
-
-        return Arrays.asList(typesMap.keys().toArray(String));
+        getMappings().get(dbName).collect { it.key }
     }
 
     @Override
     List<String> getFieldNames(String databaseName, String tableName) {
         LOGGER.debug("Executing getFieldNames for index " + databaseName + " type " + tableName)
+        getMappings().get(databaseName).get(tableName).getSourceAsMap().get('properties').collect { it.key }
+    }
 
-        Client client = getClient()
+    private static SearchSourceBuilder createSearchSourceBuilder(Query params) {
+        new SearchSourceBuilder()
+            .explain(false)
+            .from((params?.offsetClause ? params.offsetClause.offset : 0) as int)
+            .size((params?.limitClause ? params.limitClause.limit : 45) as int)
+    }
 
-        GetMappingsRequest request = new GetMappingsRequest().indices(databaseName).types(tableName)
-        GetMappingsResponse mappingResponse = client.admin().indices().getMappings(request).get()
-        HashMap fields = mappingResponse.mappings().get(databaseName).get(tableName).getSourceAsMap().get('properties')
-
-        return new ArrayList<String>(fields.keySet());;
+    private static SearchRequest createSearchRequest(SearchSourceBuilder source, Query params) {
+        new SearchRequest()
+            .searchType(SearchType.DFS_QUERY_THEN_FETCH)
+            .source(source)
+            .indices(params?.filter?.databaseName ?: '_all')
     }
 
     List<ArrayCountPair> getArrayCounts(String databaseName, String tableName, String field, int limit = 40) {
-        return null
+        getClient()
+            .search(createSearchRequest(
+                createSearchSourceBuilder(new Query(filter: new Filter(databaseName: databaseName)))
+                    .aggregation(AggregationBuilders.terms("arrayCount")
+                            .field(field)
+                            .size(limit))
+                , null))
+            .actionGet()
+            .getAggregations()
+            .asList()
+            .head()
+            .getBuckets()
+            .collect { new ArrayCountPair(key: it.key, count: it.docCount) }
     }
 
     private Client getClient() {
@@ -190,7 +209,11 @@ class ElasticSearchQueryExecutor extends AbstractQueryExecutor {
     }
 
     private ImmutableOpenMap getMappings() {
-        getClient().admin.indices.getMappings({}).actionGet().mappings
+        getClient().admin().indices().getMappings(new GetMappingsRequest()).actionGet().mappings
+    }
+
+    private static boolean isCountAllAggregation(clause) {
+        clause.operation == 'count' && clause.field == '*'
     }
 
     private static resolveSortClause(clause) {
@@ -198,23 +221,30 @@ class ElasticSearchQueryExecutor extends AbstractQueryExecutor {
         SortBuilders.fieldSort(clause.fieldName).order(order)
     }
 
-    private static resolveSingleValueAgg(clause) {
-        if (clause.operation in ['avg', 'sum', 'min', 'max']) {
-            return {
-                switch (clause.operation) {
-                case 'avg':
-                    return it.&avg
-                case 'sum':
-                    return it.&sum
-                case 'min':
-                    return it.&min
-                case 'max':
-                    return it.&max
-                }
-            }.call(AggregationBuilders)(clause.name).field(clause.field)
+    private static resolveGroupByClause(clause) {
+        if (clause.getClass() == GroupByFieldClause) {
+            return AggregationBuilders.terms(clause.field).field(clause.field as String)
         }
 
-        throw new RuntimeException("${clause.operation} is not a valid aggregation")
+        if (clause.getClass() == GroupByFunctionClause && clause.operation in ['year', 'month', 'dayOfMonth', 'hour', 'minute', 'second']) {
+            def fn = AggregationBuilders.dateHistogram(clause.name).field(clause.field as String).&interval
+            switch(clause.operation) {
+            case 'year':
+                return fn(Interval.YEAR)
+            case 'month':
+                return fn(Interval.MONTH)
+            case 'dayOfMonth':
+                return fn(Interval.DAY)
+            case 'hour':
+                return fn(Interval.HOUR)
+            case 'minute':
+                return fn(Interval.MINUTE)
+            case 'second':
+                return fn(Interval.SECOND)
+            }
+        }
+
+        throw new RuntimeException("${clause} is not a valid bucket aggregation")
     }
 
     private static createWhereClausesForFilters(DataSet dataSet, filterCache, ignoredFilterIds = []) {
@@ -238,7 +268,7 @@ class ElasticSearchQueryExecutor extends AbstractQueryExecutor {
 
     private static resolveBooleanWhereClause(clause, combiner) {
         def inners = clause.whereClauses.collect(ElasticSearchQueryExecutor.&resolveWhereClause) as FilterBuilder[]
-        FilterBuilders.boolFilter().must(combiner(inners))
+        FilterBuilders.boolFilter().must(combiner(inners) as FilterBuilder)
     }
 
     private static resolveSingularWhereClause(clause) {
@@ -263,10 +293,64 @@ class ElasticSearchQueryExecutor extends AbstractQueryExecutor {
         }
 
         if (clause.operator in ['=', '!=']) {
-            def termFilter = FilterBuilders.termFilter(clause.lhs, clause.rhs)
+            def termFilter = FilterBuilders.termFilter(clause.lhs as String, clause.rhs as String)
             return clause.operator == '=' ? termFilter : FilterBuilders.notFilter(termFilter)
         }
 
         throw new RuntimeException("${clause.operator} is an invalid operator for a where clause")
+    }
+
+    private static extractMetricAggregations(aggregates, results, hitCount) {
+        def (findAllAgg, metricAggs) = aggregates.split(ElasticSearchQueryExecutor.&isCountAllAggregation)
+        def extractedAggs = metricAggs.collect({ it -> extractMetricAggregation(it, results) })
+        if (findAllAgg) {
+            def aggMap = new HashMap()
+            aggMap.put(findAllAgg[0].name, hitCount)
+            extractedAggs.push(aggMap)
+        }
+        extractedAggs
+    }
+
+    private static Map<String, Object> extractMetricAggregation(clause, aggs) {
+        def agg = aggs.get("${STATS_AGG_PREFIX}${clause.field}" as String)
+        def aggMap = new HashMap()
+        aggMap.put(clause.name, agg[clause.operation])
+        aggMap
+    }
+
+    private static extractGroupByAggregation(groupByClauses, value, metricAggs, hitCount, Map accumulator = new HashMap(), List results = new ArrayList()) {
+        if (value instanceof MultiBucketsAggregation) {
+            value.buckets.each {
+                def newAcc = new HashMap()
+                newAcc.putAll(accumulator)
+                extractGroupByAggregation(groupByClauses, it, metricAggs, hitCount, newAcc, results)
+            }
+            return results
+        }
+
+        if (value instanceof MultiBucketsAggregation.Bucket) {
+            def currentClause = groupByClauses.head()
+
+            switch (currentClause.getClass()) {
+            case GroupByFieldClause:
+                accumulator.put(currentClause.field, value.key)
+                break
+            case GroupByFunctionClause:
+                accumulator.put(groupByClauses.head().name, value.key)
+                break
+            default:
+                throw new RuntimeException("Bad implementation - ${currentClause.getClass()} is not a valid groupByClause")
+            }
+
+            if (groupByClauses.tail()) {
+                extractGroupByAggregation(groupByClauses.tail(), value.getAggregations().asList().head(), metricAggs, hitCount, accumulator, results)
+            } else {
+                def terminalAggs = value.getAggregations()
+                if (terminalAggs) {
+                    extractMetricAggregations(metricAggs, terminalAggs.asMap(), hitCount).each(accumulator.&putAll)
+                }
+                results.push(accumulator)
+            }
+        }
     }
 }
