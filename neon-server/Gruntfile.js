41732a74d1b91ece8c423fc6a5808f434ba84590
tschellenberg <thomas.schellenberg@nextcentury.com>
2015-12-17 10:50:38 -0500

    NEON-1828 Fixed differences in running with gradlew build vs. gradlew jettyRun so the neon.js file is always built correctly.

--- neon-server/Gruntfile.js
+++ neon-server/Gruntfile.js
@@ -159,6 +159,7 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-jasmine');
     grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-jscs');
 
     // hint after concatenation since the concatenated version is also hinted
