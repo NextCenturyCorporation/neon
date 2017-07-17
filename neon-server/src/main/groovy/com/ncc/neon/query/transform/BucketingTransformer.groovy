6b84fd4b790e9ae631b7da9bc622f17e2c7ee7ac
Chris Hill <chris.hill@nextcentury.com>
2017-07-17 10:29:49 -0400

    fixes bug that caused the buckets to not be created when the cient has previously asked for buckets but neon has been since reloaded. Also cleans up comments and makes a minor perfomance increase.

--- neon-server/src/main/groovy/com/ncc/neon/query/transform/BucketingTransformer.groovy
+++ neon-server/src/main/groovy/com/ncc/neon/query/transform/BucketingTransformer.groovy
@@ -25,13 +25,10 @@ import bucketing.Bucket
 
 class BucketingTransformer implements Transformer {
 
-	// QuadNode quadTree;
 	BucketingSystem bucketor;
 	def logFile;
 	def querySize = 0;
 	def aggFieldValue;
-	// String s = null;
-	// int i = 1;
 	/**	
      * Expects params to be an object of the form {
      *       latField: string, giving the name of the latitude field in returned records,
@@ -46,60 +43,43 @@ class BucketingTransformer implements Transformer {
      *   }
      */
 	@Override
-	QueryResult convert(QueryResult queryResult, def params) {//*
+	QueryResult convert(QueryResult queryResult, def params) {
 		// logFile = new File('dxcvLog.txt')
 		// logFile << "\$" + String.valueOf(params.loadNew) + '\n'
-		// logFile << String.valueOf(params.intervals) + '\n'
-		// logFile << String.valueOf(params.intervals[0].getClass()) + '\n'
-		// logFile << String.valueOf(params.intervals[params.intervals.size()-1].getClass()) + '\n'
-
-		// def newQuerySize = queryResult.getData().size()
-		// if (newQuerySize != querySize && queryResult.getData() != null) {
-			// logFile << querySize + " " + newQuerySize + "\n"
-			// def querySize = newQuerySize
 
 		// note: despite the check below, if loadNew is false, then bucketor should not be null
-		if ( (params.loadNew || bucketor == null) && queryResult != null && queryResult.getData() != null) {
+		if (params.loadNew || bucketor == null) {
+			if (queryResult != null && queryResult.getData() != null) {
+				for (int i = 0; i < params.intervals.size(); i++) {
 
-			// logFile << "\$" + String.valueOf(params.intervals) + '\n'
-			for (int i = 0; i < params.intervals.size(); i++) {
-				// logFile << "\n" + String.valueOf("intClassBefore: " + params.intervals[i].getClass()) + '\n'
-				params.intervals[i] = (double)(params.intervals[i])
-				// logFile << String.valueOf("intClass After: " + params.intervals[i].getClass()) + '\n'
-				// logFile << "++" + String.valueOf(params.intervals[i]) + '\n'
+					params.intervals[i] = (double)(params.intervals[i])
+				}
+				bucketor = new BucketingSystem(params.intervals)
+				queryResult.getData().each { point ->
+					bucketor.insert(getFieldValue(point, params.lonField), getFieldValue(point, params.latField))
+				}
+				aggFieldValue = getFieldValue(queryResult.getData()[0], params.aggregationField)
 			}
-			bucketor = new BucketingSystem(params.intervals)
-			queryResult.getData().each { point ->
-				// logFile << getFieldValue(point, params.lonField) + ' ' + getFieldValue(point, params.latField) + '\n'
-				// def pointAgg = getFieldValue(point, params.aggregationField)
-				bucketor.insert(getFieldValue(point, params.lonField), getFieldValue(point, params.latField))
+			else {
+				List toReturn = [[data: []], [data: []]]
+				return new TabularQueryResult(toReturn)
 			}
-			aggFieldValue = getFieldValue(queryResult.getData()[0], params.aggregationField)
 		}
 
 		//***********************************************************************************************//
 
-		// logFile << "\n**" + String.valueOf("intrvlLength: " + params.i) + '\n'
 		def zoomLevel = -1
 		for (int i = 0; i < params.intervals.size(); i++) {
-			// logFile << "** " + String.valueOf(params.intervals[i]) + '\n'
 			if (params.i == params.intervals[i]) {
 				zoomLevel = i
 			}
 		}
-		// logFile << String.valueOf(zoomLevel) + '\n'
 
 		Bucket[] buckets = bucketor.getBucketsAtLevel(zoomLevel + 1)
-
-		// logFile << String.valueOf("bucketLength: " + buckets.length) + '\n'
-		// logFile << String.valueOf("query Length: " + mapTree.getBucketsAtLevel(zoomLevel+1).length) + '\n'
-		// logFile << String.valueOf("first bucket: " + buckets[0]) + '\n'
-		// logFile << String.valueOf(buckets) + '\n'
 
 		List<Map<String, Object>> newData = []
 
 		for(int i = 0; i < buckets.length; i++) {
-			// logFile << String.valueOf(buckets[i]) + '\n'
 			if (buckets[i] == null) {
 				break //buckets is big enough to hold every node at the given zoom level, but skips empty nodes;
 					  // All non-null items will be in order at the front, and the remainder of the items will be null.
@@ -111,121 +91,16 @@ class BucketingTransformer implements Transformer {
 			b.data << [count: buckets[i].count(),
 						(params.aggregationField): aggFieldValue,
 						centroid: [buckets[i].xCen(), buckets[i].yCen()]]
-			// logFile << String.valueOf(b.data) + " " + String.valueOf(b.centroid) + '\n'
 			newData << b
-			// if (centroid != null || numInBox != 0) {
-			// 	logFile << b.left + " " + b.right + " " + b.bottom + " " + b.top +  "    " + b.centroid +  "    " + numInBox + "\n"
-			// }
 		}
 
 		//The first few lines of the updateData function handle the ouput of this
 		//Changed so as to not return the entirety of the input data in addition to the buckets, for no good reason.
 		//It now returns the buckets and the size of the original data
-		List toReturn = [[data: newData], [data: newData]]
+		List toReturn = [[data: []], [data: newData]]
 		// 
 		// List toReturn = [[data: queryResult.getData()], [data: newData]]
 		return new TabularQueryResult(toReturn)
-
-		//These were used for all three variations below
-		// int numHorizTiles = Math.round(params.numTilesHorizontal)
-		// int numVertTiles = Math.round(params.numTilesVertical)
-		// double boxWidth = Math.abs(params.maxLon - params.minLon) / numHorizTiles
-		// double boxHeight = Math.abs(params.maxLat - params.minLat) / numVertTiles
-		// Map<String, Object>[][] buckets = makeBuckets(params.minLat, params.maxLat, params.minLon, params.maxLon, numHorizTiles, numVertTiles)
-
-		//***********************************************************************************************//
-
-		// queryResult.getData().each { point ->
-		// 	def pointAgg = getFieldValue(point, params.aggregationField)
-		// 	Set boxes = determineBoxes(getFieldValue(point, params.latField), getFieldValue(point, params.lonField), 
-		// 								params.maxLat, params.minLon, boxWidth, boxHeight)
-		// 	boxes.each { box ->
-		// 		if(box.horizBox < 0 || box.horizBox >= numHorizTiles || box.vertBox < 0 || box.vertBox >= numVertTiles) {
-		// 			return
-		// 		}
-		// 		def addTo = buckets[box.horizBox][box.vertBox].data.find { getFieldValue(it, params.aggregationField) == pointAgg }
-		// 		if(addTo) {
-		// 			addTo.count += 1
-		// 		}
-		// 		else {
-		// 			buckets[box.horizBox][box.vertBox].data << [count: 1, (params.aggregationField): pointAgg]
-		// 		}
-		// 	}
-		// }
-
-		//***********************************************************************************************//
-
-		// queryResult.getData().each { point ->
-		// 	def pointAgg = getFieldValue(point, params.aggregationField)
-		// 	Set boxes = determineBoxes(getFieldValue(point, params.latField), getFieldValue(point, params.lonField), 
-		// 								params.maxLat, params.minLon, boxWidth, boxHeight)
-		// 	boxes.each { box ->
-		// 		if(box.horizBox < 0 || box.horizBox >= numHorizTiles || box.vertBox < 0 || box.vertBox >= numVertTiles) {
-		// 			return
-		// 		}
-		// 		Map<String, Object> b = buckets[box.horizBox][box.vertBox];
-		// 		def addTo = b.data.find { getFieldValue(it, params.aggregationField) == pointAgg }
-		// 		if(addTo) {
-		// 			addTo.count += 0;//quadTree.searchRegion(b.left, b.right, b.top, b.bottom)
-		// 		}
-		// 		else {
-		// 			b.data << [count: quadTree.searchRegion(b.left, b.right, b.top, b.bottom), (params.aggregationField): pointAgg]
-		// 		}
-		// 		Object centroid = quadTree.getCentroidOfRegion(b.left, b.right, b.top, b.bottom)
-		// 		if (centroid == null) {
-		// 			// b.centroid = [(b.right-b.left)/2,
-		// 			// 			  (b.bottom-b.top)/2]
-		// 			b.centroid = [0,0]
-		// 		}
-		// 		else {
-		// 			b.centroid = [centroid.x(), centroid.y()]
-		// 		}
-		// 	}
-		// }
-
-
-		//***********************************************************************************************//
-
-		// def pointAgg = getFieldValue(queryResult.getData().first(), params.aggregationField)
-		// def logFile = new File('dxcvLog.txt')
-		// logFile << quadTree.getTreeString()
-
-		// for(int x = 0; x < numHorizTiles; x++) {
-		// 	for(int y = 0; y < numVertTiles; y++) {
-		// 		Map<String, Object> b = buckets[x][y]
-		// 		long numInBox = quadTree.searchRegion(b.left, b.right, b.bottom, b.top)
-		// 		// if (numInBox == 0) {
-		// 		// 	continue
-		// 		// }
-
-		// 		b.data << [count: numInBox, (params.aggregationField): ""]
-		// 		def centroid = quadTree.getCentroidOfRegion(b.left, b.right, b.bottom, b.top)
-				
-		// 		if (centroid == null) {
-		// 			b.centroid = [0,0]
-		// 		}
-		// 		else {
-		// 			b.centroid = [centroid.x(), centroid.y()]
-		// 		}
-		// 		// if (centroid != null || numInBox != 0) {
-		// 		// 	logFile << b.left + " " + b.right + " " + b.bottom + " " + b.top +  "    " + b.centroid +  "    " + numInBox + "\n"
-		// 		// }
-		// 	}
-		// }
-
-
-		//***********************************************************************************************//
-
-		// List<Map<String, Object>> newData = []
-		// for(int x = 0; x < numHorizTiles; x++) {
-		// 	for(int y = 0; y < numVertTiles; y++) {
-		// 		if(buckets[x][y].data) {
-		// 			newData << buckets[x][y] // THIS IS ONLY A STOPGAP MEASURE. MAKING FEWER BOXES WOULD BE IDEAL. TODO
-		// 		}
-		// 	}
-		// }
-		// List toReturn = [[data: queryResult.getData()], [data: newData]]
-		// return new TabularQueryResult(toReturn)
 	}
 
 	@Override
@@ -233,31 +108,32 @@ class BucketingTransformer implements Transformer {
 		return BucketingTransformer.name
 	}
 
-	private Map<String, Object>[][] makeBuckets(double minLat, double maxLat, double minLon, 
-												double maxLon, double numHorizTiles, double numVertTiles) {
-		Map<String, Object>[][] buckets = new Map<String, Object>[numHorizTiles][numVertTiles]
-		for(int x = 0; x < numHorizTiles; x++) {
-			for(int y = 0; y < numVertTiles; y++) {
-				double l = minLon + (x / numHorizTiles) * Math.abs(maxLon - minLon)
-				double r = minLon + ((x + 1) / numHorizTiles) * Math.abs(maxLon - minLon)
-				double t = maxLat - (y / numVertTiles) * Math.abs(maxLat - minLat)
-				double b = maxLat - ((y + 1) / numVertTiles) * Math.abs(maxLat - minLat)
-				buckets[x][y] = [
-					left: l,
-					right: r,
-					top: t,
-					bottom: b,
-					centroid: [ 0,
-								0],
-					// centroidX: (r - l) / 2,
-					// centroidY: (t - b) / 2,
-					data: []
 
-				]
-			}
-		}
-		return buckets
-	}
+	// private Map<String, Object>[][] makeBuckets(double minLat, double maxLat, double minLon, 
+	// 											double maxLon, double numHorizTiles, double numVertTiles) {
+	// 	Map<String, Object>[][] buckets = new Map<String, Object>[numHorizTiles][numVertTiles]
+	// 	for(int x = 0; x < numHorizTiles; x++) {
+	// 		for(int y = 0; y < numVertTiles; y++) {
+	// 			double l = minLon + (x / numHorizTiles) * Math.abs(maxLon - minLon)
+	// 			double r = minLon + ((x + 1) / numHorizTiles) * Math.abs(maxLon - minLon)
+	// 			double t = maxLat - (y / numVertTiles) * Math.abs(maxLat - minLat)
+	// 			double b = maxLat - ((y + 1) / numVertTiles) * Math.abs(maxLat - minLat)
+	// 			buckets[x][y] = [
+	// 				left: l,
+	// 				right: r,
+	// 				top: t,
+	// 				bottom: b,
+	// 				centroid: [ 0,
+	// 							0],
+	// 				// centroidX: (r - l) / 2,
+	// 				// centroidY: (t - b) / 2,
+	// 				data: []
+
+	// 			]
+	// 		}
+	// 	}
+	// 	return buckets
+	// }
 
 	private def getFieldValue(def point, String fieldName) {
 		if(point[fieldName] != null) {
