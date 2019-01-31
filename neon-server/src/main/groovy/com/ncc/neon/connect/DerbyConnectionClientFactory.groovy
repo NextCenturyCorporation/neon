3f4fe35f87daa496582944738cf49297358bc864
Thomas Schellenberg <thomas.schellenberg@nextcentury.com>
2019-01-31 10:54:22 -0500

    Fixed derby bug in tests.

--- neon-server/src/main/groovy/com/ncc/neon/connect/DerbyConnectionClientFactory.groovy
+++ neon-server/src/main/groovy/com/ncc/neon/connect/DerbyConnectionClientFactory.groovy
@@ -17,12 +17,9 @@
 package com.ncc.neon.connect
 
 class DerbyConnectionClientFactory implements ConnectionClientFactory{
-	static {
-		System.setProperty("derby.system.home", System.getProperty("catalina.base"))
-	}
-
     @Override
     ConnectionClient createConnectionClient(ConnectionInfo info) {
+        System.setProperty("derby.system.home", System.getProperty("catalina.base"))
         Boolean memoryOnly = ("true".equalsIgnoreCase(System.getProperty("derby.memoryOnly")))
         return new DerbyConnectionClient(info, memoryOnly)
     }
